5704d5ce52dd26fc556735a8c3e28b2cbc4afc0a
JeanCarlosSC <jeancarlosodi@gmail.com>
2023-09-07 13:58:56 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import view.Ventana;
+import view.Vista2;
+
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana1 = new Ventana();
+        Vista2 ventana = new Vista2();
+    }
+}
